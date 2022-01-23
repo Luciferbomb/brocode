c5bc3b11b457191469d97d6f595c641ac43e14de
Luciferbomb <60385402+Luciferbomb@users.noreply.github.com>
2022-01-24 02:58:22 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,37 @@
+
+$( document ).ready(function() {
+    var Staticroot="http://localhost:3000";
+    var Name2;
+    var Name2;
+    $(".Score").hide();
+    $("#customerOrder").on( 'click',function(){
+
+        console.log("Score");
+
+         Name1=$("[name=YourName]").val();
+         Name2=$("[name=BroName]").val();
+        console.log("Score",Name2);
+        var Score = Math.abs(((10-(Name1.length+Name2.length))/10))*100;
+        console.log("Score",Score);
+        let element = document.getElementById("Score");
+        element.innerHTML = Score;
+        $(".contact-us").hide();
+        $(".Score").show();
+    });
+    var settings = {
+        "url": Staticroot+"/create",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+          "yname": Name2,
+          "bname": Name2
+        }),
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+})
